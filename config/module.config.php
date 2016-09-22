d544f3c7c3acda901bdc136a6ee7074d471d3553
Rod McNew <rmcnew@relivinc.com>
2016-09-22 10:38:07 -0500

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -4,23 +4,16 @@ return [
         'thisControllersRoute' => '/from-app',
         'cookieName' => 'from-app-url-prepend',
         'toUrls' => [
-            'United States' => 'https://reliv.com',
-            'Australia' => 'https://relivasiapacific.com.au',
-            'Canada' => 'https://relivinc.ca',
-            'Indonesia' => 'https://relivasiapacific.co.id',
-            'Malaysia' => 'https://relivasiapacific.com.my',
-            'New Zealand' => 'https://relivasiapacific.co.nz',
-            'Philippines' => 'https://relivasiapacific.com.ph',
-            'Singapore' => 'https://relivasiapacific.com.sg',
+            'United States' => '',
+            'Australia' => '',
+            'Canada' => '',
+            'Indonesia' => '',
+            'Malaysia' => '',
+            'New Zealand' => '',
+            'Philippines' => '',
+            'Singapore' => '',
         ]
     ],
-//    'controllers' => [
-//        'config_factories' => [
-//            'MobileAppToSite\Controller\MobileAppToSiteController' => [
-//                'arguments' => ['Config']
-//            ]
-//        ]
-//    ],
     'view_manager' => [
         'template_path_stack' => [
             __DIR__ . '/../view',
